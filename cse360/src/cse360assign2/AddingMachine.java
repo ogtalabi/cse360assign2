ae3e94e7d678787da5223386f794d9f6721733ef
Seun <Seun@Medulla>
2019-10-08 22:25:43 -0700

    Initial version of AddingMachine

---
+++ cse360/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,41 @@
+package cse360assign2;
+/*Author: Oluwaseun G. Talabi
+ *Class ID:70642
+ * Assignment: #2
+ * Description: This class performs a series of basic calculations based on what is inputed and computes
+ * the addition of the parameters, the subtraction of parameters from the total, turning the operations 
+ * performed into a string, and lastly clearing the parameters and total.
+ */
+public class AddingMachine {
+
+
+	private int total;
+
+	public AddingMachine () //class constructor to instantiate total to a value
+	{
+		total = 0;  // not needed - included for clarity
+	}
+
+	public int getTotal () // @return the current total
+	{
+		return 0; //@returns the value of total
+	}
+
+	public void add (int value) //method used to add the values together into total
+	{
+	}
+
+	public void subtract (int value) //subtract the parameter from the total variable
+	{
+	}
+
+	public String toString () //@returns the operations performed as a string.
+	{
+		return "";//@returns at string of operations 
+	}
+
+	public void clear() //resets the values of the parameters and total
+	{
+	}
+
+}
